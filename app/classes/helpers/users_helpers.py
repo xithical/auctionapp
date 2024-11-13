276fc51b0075092f096a10b46d283b948839ce09
xithical <86810816+xithical@users.noreply.github.com>
2024-11-13 14:46:47 -0600

    add account page controller

--- app/classes/helpers/users_helpers.py
+++ app/classes/helpers/users_helpers.py
@@ -12,7 +12,7 @@ class Users_Helpers():
         return PasswordHasher.hash(password)
 
     @staticmethod
-    def reset_password(self, user_id: str, password: str):
+    def reset_password(user_id: str, password: str):
         user_obj = Users_Methods.get_user_by_id(user_id)
         pass_hash = Users_Helpers.hash_password(password)
         user_obj.user_password = pass_hash
