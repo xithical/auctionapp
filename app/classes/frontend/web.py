91300932fc7640c5853f2ff85bc02b607f82eb12
xithical <86810816+xithical@users.noreply.github.com>
2024-11-16 21:51:41 -0600

    start web.py

---
+++ app/classes/frontend/web.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify, request, redirect, send_from_directory
+
+from app.classes.helpers.config_helpers import Config_Helpers
+
+app = Flask(__name__, template_folder='app/classes/frontend/templates')
+
+@app.route('/', methods=['GET'])
+def entrypoint():
+    return redirect("/login")
+
+@app.route('/static/assets/<path:path>', methods=['GET'])
+def get_static_asset(path):
+    return send_from_directory('app/classes/frontend/templates/assets', path)
+
+@app.route('/login', methods=['GET','POST'])
+def user_login():
+    match request.method:
+        case 'GET':
+            org_name = Config_Helpers.get_entity_name()
+            return render_template('SignIn.html', org_name=org_name)
+        case 'POST':
+            return
