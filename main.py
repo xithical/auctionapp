fa3c267c808a2f59ae7a54c5a3404354e0236738
xithical <scottruesch02@gmail.com>
2024-11-16 22:09:20 -0600

    add flask app to main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from app.classes.models.base_model import db_proxy
 from app.classes.helpers.config_helpers import Config_Helpers
 from app.classes.helpers.init_helpers import Init_Helpers
 
+from app.classes.frontend.web import app
+
 if __name__ == "__main__":    
     Init_Helpers.init_db()
+
+    app.run()
 
     exit()
